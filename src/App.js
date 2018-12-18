df99f52a143ecb5e6c0df7238fde680165316097
papapin <papapin777#gmail.com>
2018-12-18 13:58:45 -0800

    add slider component  to header

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import React, { Component } from 'react';
 import './resources/styles.css';
 
 import Header from './components/header_footer/header';
+import Featured from './components/featured/index'
 
 
 class App extends Component {
   render() {
     return (
       <div className="App" style={{
-        height: '1000px',
+        height: '1000px', 
         background: 'green'
       }}>
        
         <Header/>
+        <Featured />
         
       </div>
     );
